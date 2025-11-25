c405f933299efa5aa9b9408a134b5ba9df21f423
codecrusader11 <shweta11042004@gmail.com>
2025-11-25 10:28:14 +0000

    Array_methods

---
+++ Basics/test.js
@@ -0,0 +1,27 @@
+// console.log("hello world")
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log('1'+2+2)
+// console.log(1+2+1+'2')
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined==0)
+// console.log(undefined > 0)
+
+//DATATYPES:
+//BigInt declaration
+// const a=349584720493827567n;
+// console.log(a);
+const Id=Symbol('123')
+const NewId=Symbol('123')
+console.log(typeof Id);
+
+//Arrays, Objects, Functions
+
+// let fruits=['apple','orange','banana']
+// let Obj={
+//     name:'lia',age:10
+// }
