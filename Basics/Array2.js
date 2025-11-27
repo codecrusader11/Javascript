57c6bb20431e66d387cb601587303fd839ef25b3
codecrusader11 <shweta11042004@gmail.com>
2025-11-27 10:25:14 +0000

    flat,conversion to array

---
+++ Basics/Array2.js
@@ -0,0 +1,26 @@
+//CONCATENATION
+let fruits=['apple','mango','banana']
+// let veges=['peas','tomato','potato']
+
+// let food=fruits.concat(veges);
+//console.log(fruits.concat(veges));
+//console.log(food)
+
+//SPREAD OPERATOR:
+//console.log(...fruits)
+// const combo=[...fruits,...veges]
+// console.log(combo)
+
+//FLAT:
+// let arr=[3,4,[5,6,1],[0,[3,1,2]],0];
+// const A=arr.flat(Infinity)
+// console.log(A)
+
+//CHECK IF ARR AND CONVERT TO ARR
+// console.log(Array.isArray("ssss"))
+// console.log(Array.isArray(fruits))
+// console.log(Array.from("tiara"))
+// console.log(Array.from({name:"tina"}))//important
+
+let A=10,B=5,C=2;
+console.log(Array.of(A,B,C))
