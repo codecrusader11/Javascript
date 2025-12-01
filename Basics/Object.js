60ab2adf2d148454793b0ea46c02a02b1dd3d266
codecrusader11 <shweta11042004@gmail.com>
2025-12-01 09:52:10 +0000

    Object Destructuring

--- Basics/Object.js
+++ Basics/Object.js
@@ -10,6 +10,11 @@ const User={
     "email":"abc@gmail.com",//can't be accessed using '.(dot)'
     [mySym]:"myKey1"
 }
+/*
+we needed to define the 'mySym' and not the other properties because it's a computed/special key and not normal object key like others
+JS converts normal ones into strings automatically. But Symbols can not be written as plain text keys. The square brackets mean 'use the 
+variable value as the key, not the literal text'.
+*/
 
 // console.log(User.age)
 // console.log(User["name"])
